5e35c196ce35d4d53605b48054e5ce83df10146e
mukhtor <47754082+mukhtor@users.noreply.github.com>
2022-03-28 14:47:15 +0500

    asda

--- backend/config/main.php
+++ backend/config/main.php
@@ -77,16 +77,12 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-
-
-        ],
-
+    ],
     'params' => $params,
 ];
