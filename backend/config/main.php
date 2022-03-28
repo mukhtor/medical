e7bbf497e579532587279aec849833f83d8ba392
mukhtor <47754082+mukhtor@users.noreply.github.com>
2022-03-28 12:35:41 +0500

    asset manager linkassets1

--- backend/config/main.php
+++ backend/config/main.php
@@ -84,61 +84,9 @@ return [
             'rules' => [
             ],
         ],
-        'assetManager' => [
-            'class' => \yii\web\AssetManager::class,
-            'bundles' => [
-                'linkAssets' => true,
-//                \yii\web\JqueryAsset::class => [
-//                    'sourcePath' => null,   // не опубликовывать комплект
-//                    'basePath' => '@webroot/assets/jquery',
-//                    'baseUrl' => '@web/assets/jquery',
-//                    'js' => [
-//                        'dist/jquery.min.js',
-//                    ]
-//                ],
-//                \hail812\adminlte3\assets\FontAwesomeAsset::class => [
-//                    'sourcePath' => null,
-//                    'basePath' => '@webroot/assets/fontawesome-free',
-//                    'baseUrl' => '@web/assets/fontawesome-free',
-//                    'css' => [
-//                        'css/all.min.css',
-//                    ]
-//                ],
-//                \hail812\adminlte3\assets\AdminLteAsset::class => [
-//                    'sourcePath' => null,
-//                    'basePath' => '@webroot/assets/adminlte',
-//                    'baseUrl' => '@web/assets/adminlte',
-//                    'css' => [
-//                        'css/adminlte.min.css',
-//                    ],
-//                    'js' => [
-//                        'js/adminlte.min.js',
-//                    ]
-//                ],
-//                \yii\bootstrap4\BootstrapAsset::class=>[
-//                    'sourcePath' => null,
-//                    'basePath' => '@webroot/assets/bootstrap',
-//                    'baseUrl' => '@web/assets/bootstrap',
-//                    'css' => [
-//                        'css/bootstrap.min.css',
-//                    ],
-//
-//                ],
-//                \yii\bootstrap4\BootstrapPluginAsset::class=>[
-//                    'sourcePath' => null,
-//                    'basePath' => '@webroot/assets/bootstrap',
-//                    'baseUrl' => '@web/assets/bootstrap',
-//                    'js' => [
-//                        'js/bootstrap.bundle.min.js',
-//                    ]
-//                ],
 
-
-
-            ],
 
         ],
 
-    ],
     'params' => $params,
 ];
